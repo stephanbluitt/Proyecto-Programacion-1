ba03e5270601f32753aaf14cb5e9e6a84e1da161
stephanbluitt <46945764+stephanbluitt@users.noreply.github.com>
2020-03-26 21:38:51 -0400

    la implementacion de crear la vacunas y enfermededades que e arrojan al arrayList, basicamente la creacion de todos los elementos con sus prarametro en los 5 grandes (paciente, doctor, enfermedad, vacuna)

--- proyectofinalP1/src/Logico/Main.java
+++ proyectofinalP1/src/Logico/Main.java
@@ -1,34 +1,56 @@
 package Logico;
 
 public class Main {
-	static Clinica clini = new Clinica();
+	 static Clinica clini = new Clinica();
 	
 	
 	public static void main(String[] args) {	
 		
 		crearPaciente("12345678901","juan","peres","el real final calle 10","1234567890","juan@hfj.com","11","22","22","masculino","trabajador");//ACÁ SE PASAN Y CREAN LOS DATOS PARA UN PACIENTE
+		crearDoctor("12345678901","juan","peres","el real final calle 10","1234567890","juan@hfj.com","11","22","22","masculino","otorrino");
+		crearEnfermedad("covi19","lo que causa");
+		crearVacuna("ebola","lol");
 		//DONDE SE PASAN LOS DATOS DE CREAR UN PACIENTE
-		//true= se creal el paciente. false= Debe revisar los datos que entro, no se creo
+		
 	}
 	
-	
-	public static boolean crearPaciente(String id,String nombre,String apellido,String direccion,String telefono,String email,String dia,String mes,String ano,String sexo,String ocupacion){
+	//BASICAMENTE SE LE PASAN LOS PARAMETRO Y SI ESTOS SON VERDADEROS, SE PROCEDE A CREAR EL REGISTRO, SE DOCTOR O PACIENTE
+
+//INGRESAR PACIENTE	
+	private static boolean crearPaciente(String id,String nombre,String apellido,String direccion,String telefono,String correoElectronico,String dia,String mes,String ano,String sexo,String ocupacion){
 		//String id limite de 11 digitos con guion automatizado a medida que se escribe, parte visual, pero se pasa aqui sin guion 
 		//fecha de nacimiento = dia+"-"+mes+"-"ano
 		//fecha CON 3 SPINNERS, antes de ingresar todo, no debe permitir que todo este vacio.		
 		String fechaNacimiento = dia+"-"+mes+"-"+ano;
-		if (validarPersona(id,nombre,apellido,direccion,telefono,email,fechaNacimiento,sexo,ocupacion)==false) {
+		if (validarPersona(id,nombre,apellido,direccion,telefono,correoElectronico,fechaNacimiento,sexo,ocupacion)==false) {
 			//se imprimiria que hay un dato errado
 			return false; 
 		}else {
-			Paciente paci  = new Paciente("123","jose","alcachofaz","su casita","123123321", "hcbeh@ceo.com","12-31-1998","masculino", "ninguna");
+			Paciente paci  = new Paciente(id,nombre,apellido,direccion,telefono,correoElectronico,fechaNacimiento,sexo,ocupacion);
 			clini.insertarPaciente(paci);
 			//si da true se
 			return true;
 		}
-	}
+	}	
+//INGRESAR DOCTOR
+	private static boolean crearDoctor(String id,String nombre,String apellido,String direccion,String telefono,String correoElectronico,String dia,String mes,String ano,String sexo,String especialidad){
+		//String id limite de 11 digitos con guion automatizado a medida que se escribe, parte visual, pero se pasa aqui sin guion 
+		//fecha de nacimiento = dia+"-"+mes+"-"ano
+		//fecha CON 3 SPINNERS, antes de ingresar todo, no debe permitir que todo este vacio.		
+		String fechaNacimiento = dia+"-"+mes+"-"+ano;
+		if (validarPersona(id,nombre,apellido,direccion,telefono,correoElectronico,fechaNacimiento,sexo,especialidad)==false) {
+			//se imprimiria que hay un dato errado
+			return false; 
+		}else {
+			Doctor doc  = new Doctor(id,nombre,apellido,direccion,telefono,correoElectronico,fechaNacimiento,sexo,especialidad);
+			clini.insertarDoctor(doc);
+			//si da true se
+			return true;
+		}
+	}	
+//VALIDAR PERSONA
 	private static boolean validarPersona(String id, String nombre, String apellido, String direccion, String telefono,
-			String email, String fechaNacimiento, String sexo, String ocupacion) {
+			String correoElectronico, String fechaNacimiento, String sexo, String ocupacion) {
 	//revisar formatos si se entraron correctamente
 	//id o cedula
 		
@@ -55,5 +77,14 @@ public class Main {
 		}
 		return true;
 		}
-
+//INGRESAR ENFERMEDAD
+	private static void crearEnfermedad(String nombreEnfermedad, String descripcionEnfermedad){
+		ControlEnfermedades aux = new ControlEnfermedades(nombreEnfermedad, descripcionEnfermedad);
+		clini.insertarEnfermedadBajoVigilancia(aux);	
+	}	
+//INGRESAR VACUNA
+	private static void crearVacuna(String nombreVacuna, String descripcionVacuna){
+		ControlVacunacion aux = new ControlVacunacion(nombreVacuna, descripcionVacuna);
+		clini.insertarVacunasbajovigilancia(aux);	
+	}		
 }
